465474af3a9fc70915e20f72bbbcfea5f4409c89
test68 <test68@email.address>
2019-06-17 13:19:17 +0800

    Day1

---
+++ lesson.js
@@ -0,0 +1,13 @@
+let $sds = 123;
+document.write($sds + "<br>");
+var1 = 13213;
+document.write(typeof(var1) + "<br>");
+var1 = 132.13;
+document.write(typeof(var1) + "<br>");
+var1 = true;
+document.write(typeof(var1) + "<br>");
+
+const name = "Brad";
+document.write(`my name is ${name}<br>`);
+let a = 10, b =3;
+document.write(`${a} + ${b} = ${a+b}`);
